5350d35103157f6582bf40ece5ceb24d97f07894
Unknown <chenhaichao@kavout.com>
2022-07-02 17:26:13 +0800

    更改端口

--- config.py
+++ config.py
@@ -5,6 +5,9 @@ DEBUG = True
 
 # 读取数据库环境变量
 username = os.environ.get("MYSQL_USERNAME", 'root')
-password = os.environ.get("MYSQL_PASSWORD", '12345678')
-db_address = os.environ.get("MYSQL_ADDRESS", '127.0.0.1:3306')
+# password = os.environ.get("MYSQL_PASSWORD", '12345678')
+password = os.environ.get("MYSQL_PASSWORD", 'p29Am6vX')
+# db_address = os.environ.get("MYSQL_ADDRESS", '127.0.0.1:3306')
+# 正式环境的域名以及端口
+db_address = os.environ.get("MYSQL_ADDRESS", 'sh-cynosdbmysql-grp-333sy8t0.sql.tencentcdb.com:26849')
 # print(username, password, db_address)
